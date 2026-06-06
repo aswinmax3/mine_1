d06a0951b3c8249967b23531561ef8ebe4424921
aswinmax3 <aswinmax3@gmail.com>
2026-06-06 17:19:58 +0530

    Fix ALLOWED_HOSTS

--- insurance_ai/settings.py
+++ insurance_ai/settings.py
@@ -43,12 +43,7 @@ SECRET_KEY = 'django-insecure-ud$lucdckk+*76y6s5&u(y84m_xzq2zq+73eglokz6^lo#so&=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-
-ALLOWED_HOSTS = [
-    "policy-decode-ai.onrender.com",
-    "localhost",
-    "127.0.0.1",
-]
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
